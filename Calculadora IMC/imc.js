1467dc80f16f1cfe56cb459207efac05bc6abc0e
Mauricio González Marulanda <65740677+mauriciogm123m@users.noreply.github.com>
2022-11-19 11:48:00 -0500

    Add files via upload

---
+++ Calculadora IMC/imc.js
@@ -0,0 +1,41 @@
+function calcularImc()
+{
+    let altura = document.getElementById('altura').value
+    let peso = document.getElementById('peso').value 
+    let img = document.getElementById('img')
+
+    let imc = peso / (altura * altura).toFixed(2)
+
+    if(imc >=0 && imc < 18.5)
+    {
+        img.src = 'img/magro.png'
+        document.body.style.background = '#ca9292' 
+    }
+    else if (imc >= 18.5 && imc <24.9)
+    {
+        img.src = 'img/peso-ideal.png'
+        document.body.style.background = 'rgb(137 137 137)' 
+    }
+    else if (imc >= 25 && imc <30)
+    {
+        img.src = 'img/acima do peso.png'
+        document.body.style.background = '#f1937e' 
+    }
+    else if (imc >= 30.1 && imc <35)
+    {
+        img.src = 'img/gordo.png'
+        document.body.style.background = '#f1937e' 
+    }
+    else if (imc >= 35.1 && imc <99)
+    {
+        img.src = 'img/obeso.png'
+        document.body.style.background = '#815e4c' 
+    }
+    else{imc
+    }
+
+}
+
+
+let button = document.getElementById('button')
+button.addEventListener('click', calcularImc)
